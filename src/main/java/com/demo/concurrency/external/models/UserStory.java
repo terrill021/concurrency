cc865e8405a0e104e079a46c9cf72b894a4b3fb5
Alejandro Terrill <Alejandro.X.Terril.-ND@disney.com>
2020-11-03 10:02:37 -0500

    ejercisio 1. Adaptar rxjava.

--- src/main/java/com/demo/concurrency/external/models/UserStory.java
+++ src/main/java/com/demo/concurrency/external/models/UserStory.java
@@ -19,15 +19,7 @@ public class UserStory {
         return user;
     }
 
-    public void setUser(User user) {
-        this.user = user;
-    }
-
     public List<Post> getPosts() {
         return posts;
-    }
-
-    public void setPosts(List<Post> posts) {
-        this.posts = posts;
     }
 }
